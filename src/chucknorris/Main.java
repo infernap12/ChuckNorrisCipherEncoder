542c9a0a9fdc682dae233c41404836142e159d60
james <james1.6@hotmail.com>
2023-12-26 07:15:29 +1030

    Stage 1 - Solution

--- src/chucknorris/Main.java
+++ src/chucknorris/Main.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Input string:");
         String input = scanner.nextLine();
         for (char ch : input.toCharArray()) {
-            System.out.println(ch + " ");
+            System.out.print(ch + " ");
 
         }
+        System.out.println();
     }
 }
