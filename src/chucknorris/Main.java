090c720ecb12e24725014397dfeb6fafcce96dfd
james <james1.6@hotmail.com>
2023-12-26 15:21:36 +1030

    Stage 2 - Solution

--- src/chucknorris/Main.java
+++ src/chucknorris/Main.java
@@ -1,16 +1,20 @@
 package chucknorris;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        String input;
         System.out.println("Input string:");
-        String input = scanner.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        input = scanner.nextLine();
+        System.out.println("The result:");
         for (char ch : input.toCharArray()) {
-            System.out.print(ch + " ");
+            String chstring = String.format("%7s", Integer.toBinaryString((int)ch)).replace(' ','0');
+            System.out.printf("%c = %7s%n", ch, chstring);
 
         }
-        System.out.println();
+
     }
 }
