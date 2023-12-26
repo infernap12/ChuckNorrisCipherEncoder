394ab06877b2e5c44e15c07199cc1adbdb25a09b
james <james1.6@hotmail.com>
2023-12-26 17:17:40 +1030

    Stage 3 - Norris Encoding

--- src/chucknorris/Main.java
+++ src/chucknorris/Main.java
@@ -1,20 +1,61 @@
 package chucknorris;
 
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        String input;
+    public static void main(String[] args) {
         System.out.println("Input string:");
-        Scanner scanner = new Scanner(System.in);
-        input = scanner.nextLine();
+        String binaryString = getBinaryString();
         System.out.println("The result:");
-        for (char ch : input.toCharArray()) {
-            String chstring = String.format("%7s", Integer.toBinaryString((int)ch)).replace(' ','0');
-            System.out.printf("%c = %7s%n", ch, chstring);
+        String norrisMsg = getNorrisMsg(binaryString);
+        System.out.println(norrisMsg);
+    }
+
+    private static String getNorrisMsg(String binaryString) {
+        StringBuilder norrisMsg = new StringBuilder();
+        char previousChar = binaryString.charAt(0);
+        int run = 1;
+        String symbol = previousChar == '1' ? "0" : "00";
+        for (int i = 1; i < binaryString.length(); i++) {
+            if (binaryString.charAt(i) == previousChar) {
+                run++;
+            } else {
+                norrisMsg.append("%s %s ".formatted(symbol,"0".repeat(run)));
+
+                symbol = symbol.equals("0") ? "00" : "0"; //swap symbols
+                previousChar = binaryString.charAt(i);
+                run = 1;
+            }
 
         }
+        norrisMsg.append("%s %s ".formatted(symbol,"0".repeat(run)));
 
+
+
+
+        return norrisMsg.toString().trim();
+    }
+
+    private static String getBinaryString() {
+        String inputLine;
+        Scanner scanner = new Scanner(System.in);
+        inputLine = scanner.nextLine();
+
+        StringBuilder binaryString = new StringBuilder();
+
+        for (char ch : inputLine.toCharArray()) {
+            String chstring = String.format("%7s", Integer.toBinaryString(ch)).replace(' ','0');
+            binaryString.append(chstring);
+
+        }
+        return binaryString.toString();
     }
 }
+
+
+
+/*
+C
+1000011
+0 0 00 0000 0 00
+ */
